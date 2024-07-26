b058a0a72de9c0aaa2dd531e2edeee5cdef7283f
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-07-26 11:41:18 +0000

    Working with block scope variables

---
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable */
+
+export default function taskBlock(trueOrFalse) {
+  var task = false;
+  var task2 = true;
+
+  if (trueOrFalse) {
+    let task = true;
+    let task2 = false;
+  }
+
+  return [task, task2];
+}
