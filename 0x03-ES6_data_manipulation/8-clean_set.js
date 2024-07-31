00189fb9a197d86de8ace430e80c38c8576f0729
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-07-31 14:25:41 +0000

    BUG: Error when non-string are passed as arguments.

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,11 +1,15 @@
 export default function cleanSet(set, startString) {
   let newString = '';
 
-  for (const val of set) {
-    if (startString.length > 0 && val.startsWith(startString)) {
-      newString = `${newString}-${val.substring(3)}`;
+  if (startString && typeof startString === 'string' && startString.length > 0) {
+    for (const val of set) {
+      if (startString.length > 0 && val.startsWith(startString)) {
+        newString = `${newString}-${val.substring(3)}`;
+      }
     }
+
+    newString = newString.replace('-', '');
   }
 
-  return newString.replace('-', '');
+  return newString;
 }
