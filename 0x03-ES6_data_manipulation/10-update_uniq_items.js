fc21b154105ff1ef276b4cadcf3c669bd55aa723
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-08-02 20:47:12 +0000

    BUG: throw error

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,4 +1,8 @@
 export default function updateUniqueItems(mapObj) {
+  if (!(mapObj instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+
   for (const [key, value] of mapObj) {
     if (value === 1) {
       mapObj.set(key, value * 100);
