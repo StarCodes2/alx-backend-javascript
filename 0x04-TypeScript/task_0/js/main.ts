ca9c561f49e004f020350abdbcab8add8ae9116d
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-08-03 22:21:24 +0000

    Task-0

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -21,3 +21,24 @@ const student2: Student = {
 
 const studentsList: Student[] = [studenet1, student2];
 
+document.addEventListener('DOMContentLoaded'. function() {
+  const table = document.createElement('table');
+  const tbody = document.createElement('tbody');
+
+  studentsList.forEach((student) => {
+    const row = document.createElement('tr');
+
+    const firstNameCell = document.createElement('td');
+    firstNameCell.textContent = student.firstName;
+    row.appendChild(firstNameCell);
+
+    const locationCell = document.createElement('td');
+    locationCell.textContent = student.location;
+    row.appendChild(locationCell);
+
+    tbody.appendChild(row);
+  });
+
+  table.appendChild(tbody);
+  document.body.appendChild(table);
+});
