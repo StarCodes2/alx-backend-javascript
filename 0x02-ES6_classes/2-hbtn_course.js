28464d2209a27adcd4b93d67d51ee55a43478c90
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-08-06 02:28:12 +0000

    Update

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -11,7 +11,7 @@ export default class HolbertonCourse {
 
   set name(value) {
     if (typeof value !== 'string') {
-      throw new TypeError('Name must be a string');
+      throw new TypeError('Name must be a string.');
     }
     this._name = value;
   }
@@ -22,7 +22,7 @@ export default class HolbertonCourse {
 
   set length(value) {
     if (typeof value !== 'number') {
-      throw new TypeError('Length must be a number');
+      throw new TypeError('Length must be a number.');
     }
     this._length = value;
   }
@@ -33,7 +33,7 @@ export default class HolbertonCourse {
 
   set students(arr) {
     if (!Array.isArray(arr) || !arr.every((student) => typeof student === 'string')) {
-      throw new TypeError('Students must be an array of strings');
+      throw new TypeError('Students must be an array of strings.');
     }
     this._students = arr;
   }
