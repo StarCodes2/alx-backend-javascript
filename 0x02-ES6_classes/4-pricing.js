03b556dd2a91b9bdaeef4988bb27469e57e31a74
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-08-16 17:33:42 +0000

    BUG: Wrong check in set method for currency.

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -22,7 +22,7 @@ export default class Pricing {
   }
 
   set currency(value) {
-    if (!this._currency.instanceof(Currency)) {
+    if (!value.instanceof(Currency)) {
       throw new TypeError('Currency must be an instance of Currency');
     }
     this._currency = value;
