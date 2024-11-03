726ba3c607fa362fe7f7259e8b13a7288e924fa9
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-11-03 20:12:47 +0000

    To add more than one handler to a promise.

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,5 +1,5 @@
 export default function handleResponseFromAPI(promise) {
-  return promise.then(() => ({ status: 200, body: 'Success' })).then(() => {
+  return promise.then(() => Promise.resolve({ status: 200, body: 'Success' })).then(() => {
     console.log('Got a response from the API');
   }).catch(() => Error());
 }
