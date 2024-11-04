8b79d4e59e80af09c860110bab2c3834d82ea8d0
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-11-04 14:05:46 +0000

    BUG: Missing semicolon.

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -2,7 +2,7 @@ import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()]).then((res) => {
-    const [photo, user] = res
+    const [photo, user] = res;
     console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
   }).catch(() => {
     console.log('Signup system offline');
