0fbd8cd24cb8bf3a3b7880443ced0db48a8920c4
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-11-04 06:23:47 +0000

    BUG: used Success instead of success.

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,5 +1,5 @@
 export default function handleResponseFromAPI(promise) {
-  return promise.then(() => Promise.resolve({ status: 200, body: 'Success' })).finally(() => {
+  return promise.then(() => Promise.resolve({ status: 200, body: 'success' })).finally(() => {
     console.log('Got a response from the API');
   }).catch(() => Error());
 }
