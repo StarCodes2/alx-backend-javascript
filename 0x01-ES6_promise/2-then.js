921af8ffee03c3c8fdde4ef7674f24c1a6b04e53
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-11-04 06:14:19 +0000

    using finally instead of then

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,5 +1,5 @@
 export default function handleResponseFromAPI(promise) {
-  return promise.then(() => Promise.resolve({ status: 200, body: 'Success' })).then(() => {
+  return promise.then(() => Promise.resolve({ status: 200, body: 'Success' })).finally(() => {
     console.log('Got a response from the API');
   }).catch(() => Error());
 }
