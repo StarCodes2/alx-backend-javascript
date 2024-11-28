6f6c64c7f3eeca99ac536ec6a396fefac6f62448
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-11-28 14:17:42 +0000

    Using Assert to test a function

---
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+const calculateNumber = require('./0-calcul');
+
+describe('calculateNumber test', function () {
+  it('Check equality', function () {
+    assert.equal(calculateNumber(5.5, 4), 10);
+    assert.equal(calculateNumber(5, 4.6), 10);
+    assert.equal(calculateNumber(5.5, 4.2), 10);
+  });
+});
