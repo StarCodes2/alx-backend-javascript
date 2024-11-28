9037c59bca67d25aabe833c4c842d9ca086b5f30
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-11-28 21:15:16 +0000

    BUG: Value of 'b' not rounded before check if it equals zero.

--- 0x06-unittests_in_js/1-calcul.js
+++ 0x06-unittests_in_js/1-calcul.js
@@ -5,7 +5,7 @@ function calculateNumber(type, a, b) {
   if (type === 'SUBTRACT') {
     return Math.round(a) - Math.round(b);
   }
-  if (type === 'DIVIDE' && b !== 0) {
+  if (type === 'DIVIDE' && Math.round(b) !== 0) {
     return Math.round(a) / Math.round(b);
   }
   return 'Error';
