efa91e59d3e310700ce44ee175315fd4b8b9127a
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-11-28 14:19:56 +0000

    TEST: add test for integers.

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,6 +3,7 @@ const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber test', function () {
   it('Check equality', function () {
+    assert.equal(calculateNumber(5, 4), 9);
     assert.equal(calculateNumber(5.5, 4), 10);
     assert.equal(calculateNumber(5, 4.6), 10);
     assert.equal(calculateNumber(5.5, 4.2), 10);
