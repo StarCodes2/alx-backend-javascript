98c384eb67a3b9038edca0e7ff783fea34a2c21e
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-12-03 20:47:55 +0000

    To test an api with a single route.

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -2,11 +2,12 @@ const { expect } = require('chai');
 const request = require('request');
 
 describe('Test a route using request and chai', function () {
-  it('should return 200 as status code', function () {
+  it('should return 200 as status code', function (done) {
     request('http://localhost:7865/', (err, res, body) => {
       expect(err).to.equal(null);
       expect(res.statusCode).to.equal(200);
       expect(body).to.equal('Welcome to the payment system');
+      done();
     });
   });
 });
