8e01f09c454a669fcc26b7e6e5babeaabf5eb82b
Ezekiel Ogunewu <anu.ezekiel02@gmail.com>
2024-12-04 06:44:17 +0000

    To test a link that contains a variable.

---
+++ 0x06-unittests_in_js/9-api/api.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 7865;
+
+app.get('/', (req, res) => {
+ res.send('Welcome to the payment system');
+});
+
+app.get('/cart/:id', (req, res) => {
+  const id = req.params.id;
+  if (!isNaN(id) && Number(id).toString() === id) {
+    return res.send(`Payment methods for cart :${id}`);
+  }
+  return res.sendStatus(404);
+});
+
+app.listen(port, () => {
+  console.log(`API available on localhost port ${port}`);
+});
---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,22 @@
+const { expect } = require('chai');
+const request = require('request');
+
+describe('Test a route using request and chai', function () {
+  it('should return 200 as status code', function (done) {
+    request('http://localhost:7865/cart/65', (err, res, body) => {
+      expect(err).to.equal(null);
+      expect(res.statusCode).to.equal(200);
+      expect(body).to.equal('Payment methods for cart :65');
+      done();
+    });
+  });
+
+  it('should return 404 as status code', function (done) {
+    request('http://localhost:7865/cart/6y', (err, res, body) => {
+      expect(err).to.equal(null);
+      expect(res.statusCode).to.equal(404);
+      expect(body).to.not.equal('Payment methods for cart :6y');
+      done();
+    });
+  });
+});
